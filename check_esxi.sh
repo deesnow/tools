206147fa3658ec23f4b0e9936a773030771e5e49
János Tarjányi <jtarjanyi@geomant.com>
2019-06-17 11:22:46 +0200

    Initial commit

---
+++ check_esxi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function funk() {
+	echo "Esxi host : $line" >> output.txt
+	echo "-------------------------------------" >> output.txt
+	ssh -n  -oStrictHostKeyChecking=no root@$line 'esxcli system version get && esxcli network nic get -n vmnic4 && esxcli network nic get -n vmnic5 && esxcfg-advcfg -g /DataMover/HardwareAcceleratedMove && esxcfg-advcfg -g /DataMover/HardwareAcceleratedInit && esxcfg-advcfg -g /VMFS3/HardwareAcceleratedLocking' >> output.txt
+	echo "-------------------------------------" >> output.txt
+	echo "                                     " >> output.txt
+	
+}
+
+
+
+while IFS= read -r line
+do
+	echo "Logging into $line"
+	funk
+	echo "$line is DONE"
+
+done <"$1"
